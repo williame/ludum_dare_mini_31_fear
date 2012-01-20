8adc5095412362b7de59a44005779eb3d65c0e0f
Will <will@Will.(none)>
2012-01-20 15:59:05 +0100

    initial try at gh-pages

--- barebones/build_info.cpp
+++
@@ -1,4 +0,0 @@
-#include "build_info.hpp"
-
-const char* const build_timestamp = BUILD_TIMESTAMP;
-const char* const git_info = GIT_INFO;
--- barebones/build_info.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __BUILD_INFO_HPP__
-#define __BUILD_INFO_HPP__
-
-extern const char* const build_timestamp;
-extern const char* const git_info;
-
-#endif//__BUILD_INFO_HPP__
