0ccd0b237627926ad1ca43733d40dd1d111936ea
Will <will@Will.(none)>
2012-01-21 04:25:24 +0100

    you can edit floor and ceiling

--- game.cpp
+++ game.cpp
@@ -41,6 +41,7 @@ private:
 		MODE_LOAD,
 		MODE_OBJECT,
 		MODE_FLOOR,
+		MODE_CEILING,
 	} mode;
 	xml_parser_t game_xml;
 	glm::vec2 screen_centre;
@@ -62,8 +63,8 @@ const float main_game_t::PAN_RATE = 10;
 
 struct main_game_t::artwork_t: public g3d_t, public g3d_t::loaded_t {
 	enum class_t {
-		CLS_BACK = 50,
-		CLS_MONSTER = 30,
+		CLS_BACK = 150,
+		CLS_MONSTER = 90,
 	};
 	artwork_t(main_game_t& main,const std::string& id_,const std::string& p,class_t c,float s):
 		g3d_t(main,p,this), game(main), id(id_), path(p), cls(c), tx(glm::scale(glm::vec3(s,s,s))), scale_factor(s) {}
@@ -108,7 +109,7 @@ struct main_game_t::path_t {
 	struct node_t {
 		node_t(int id_,const glm::vec2& p): id(id_), pos(p) {}
 		const int id;
-		const glm::vec2 pos;
+		glm::vec2 pos;
 		links_t links;
 	};
 	struct link_t {
@@ -235,24 +236,76 @@ struct main_game_t::path_t {
 	}
 	void on_mouse_down(int x,int y,mouse_button_t button,const input_key_map_t& map,const input_mouse_map_t& mouse) {
 		const glm::vec2 pos(x,y);
-		active_node = nearest(pos);
-		if(!active_node) {
-			if(link_t* link = nearest_link(pos)) {
-				active_node = new node_t(id_seq++,pos);
-				nodes.push_back(active_node);
-				active_node->links.push_back(link);
-				node_t* b = link->b;
-				b->links.erase(std::find(b->links.begin(),b->links.end(),link));
-				link->b = active_node;
-				link = new link_t(active_node,b);
-				links.push_back(link);
-				active_node->links.push_back(link);
-				b->links.push_back(link);
+		if(button == main_t::MOUSE_DRAG) {
+			if(active_node) {
+				active_node->pos = pos;
 				dirty = true;
 			}
+		} else if(node_t* new_active_node = nearest(pos)) {
+			if(active_node) { // if they are not linked, join them
+				bool joined = false;
+				for(links_t::iterator l=active_node->links.begin(); !joined && l!=active_node->links.end(); l++)
+					joined = ((*l)->a == new_active_node) || ((*l)->b == new_active_node);
+				if(!joined) {
+					link_t* link = new link_t(active_node,new_active_node);
+					links.push_back(link);
+					active_node->links.push_back(link);
+					new_active_node->links.push_back(link);
+					dirty = true;
+				}
+			}
+			active_node = new_active_node;
+		} else if(link_t* link = nearest_link(pos)) {
+			active_node = new node_t(id_seq++,pos);
+			nodes.push_back(active_node);
+			active_node->links.push_back(link);
+			node_t* b = link->b;
+			b->links.erase(std::find(b->links.begin(),b->links.end(),link));
+			link->b = active_node;
+			link = new link_t(active_node,b);
+			links.push_back(link);
+			active_node->links.push_back(link);
+			b->links.push_back(link);
+			dirty = true;
+		} else {
+			node_t* new_active_node = new node_t(id_seq++,pos);
+			if(active_node) {
+				link_t* link = new link_t(active_node,new_active_node);
+				links.push_back(link);
+				active_node->links.push_back(link);
+				new_active_node->links.push_back(link);
+			}
+			nodes.push_back(new_active_node);
+			dirty = true;
+			active_node = new_active_node;
 		}
 	}
-	void on_mouse_up(int x,int y,mouse_button_t button,const input_key_map_t& map,const input_mouse_map_t& mouse) {
+	void on_mouse_up(int x,int y,mouse_button_t button,const input_key_map_t& map,const input_mouse_map_t& mouse) {}
+	bool on_key_down(short code,const input_key_map_t& map,const input_mouse_map_t& mouse) { return false; }
+	bool on_key_up(short code,const input_key_map_t& map,const input_mouse_map_t& mouse) {
+		switch(code) {
+		case main_t::KEY_BACKSPACE:
+			if(map.any()) return false;
+			if(active_node) {
+				for(links_t::iterator l=active_node->links.begin(); l!=active_node->links.end(); l++) {
+					link_t* link = *l;
+					links.erase(std::find(links.begin(),links.end(),link));
+					if(link->a == active_node)
+						link->b->links.erase(std::find(link->b->links.begin(),link->b->links.end(),link));
+					else if(link->b == active_node)
+						link->a->links.erase(std::find(link->a->links.begin(),link->a->links.end(),link));
+					else
+						panic("wtf");
+				}
+				nodes.erase(std::find(nodes.begin(),nodes.end(),active_node));
+				delete active_node;
+				active_node = NULL;
+				dirty = true;
+			}
+			return true;
+		default:
+			return false;
+		}
 	}
 };
 
@@ -305,7 +358,7 @@ void main_game_t::on_io(const std::string& name,bool ok,const std::string& bytes
 void main_game_t::on_ready(artwork_t*) {
 	if(is_ready()) {
 		std::cout << "artwork all loaded" << std::endl;
-		mode = MODE_OBJECT;
+		mode = MODE_FLOOR;
 		xml_walker_t xml(game_xml.walker());
 		xml.check("game").get_child("level");
 		for(int i=0; xml.get_child("object",i); i++, xml.up()) {
@@ -334,7 +387,7 @@ bool main_game_t::tick() {
 	const glm::mat4 projection(glm::ortho<float>(
 		screen_centre.x-width/2,screen_centre.x+width/2,
 		screen_centre.y-height/2,screen_centre.y+height/2, // y increases upwards
-		1,90));
+		1,200));
 	const glm::vec3 light0(10,10,10);
 	// show all the objects
 	for(objects_t::iterator i=objects.begin(); i!=objects.end(); i++)
@@ -342,7 +395,7 @@ bool main_game_t::tick() {
 	// show active model on top for editing
 	if((mode == MODE_OBJECT) && active_model && mouse_down) {
 		active_model->draw(time,projection,
-			glm::translate(glm::vec3(screen_centre.x+mouse_x-width/2,screen_centre.y-mouse_y+height/2,-20))*active_model->tx,
+			glm::translate(glm::vec3(screen_centre.x+mouse_x-width/2,screen_centre.y-mouse_y+height/2,-50))*active_model->tx,
 			light0,glm::vec4(1,.6,.6,.6));
 	}
 	// floor and ceiling
@@ -389,24 +442,33 @@ bool main_game_t::on_key_down(short code,const input_key_map_t& map,const input_
 	case KEY_RIGHT: pan_rate.x = PAN_RATE; return true;
 	case KEY_UP: pan_rate.y = PAN_RATE; return true;
 	case KEY_DOWN: pan_rate.y = -PAN_RATE; return true;
-	case 'o': mode = MODE_OBJECT; return true;
-	case 'f': mode = MODE_FLOOR; return true;
-	case ' ': {
-			if(!artwork.size()) return false;
-			bool next = false;
-			for(artworks_t::iterator m=artwork.begin(); m!=artwork.end(); m++)
-				if(next) {
-					active_model = m->second;
-					next = false;
-					break;
-				} else
-					next = m->second == active_model;
-			if(next)
-				active_model = artwork.begin()->second;
-	} return true;
-	default: return false;
+	case 'o': mode = MODE_OBJECT; std::cout << "OBJECT MODE" << std::endl; return true;
+	case 'f': mode = MODE_FLOOR; std::cout << "FLOOR MODE" << std::endl; return true;
+	case 'c': mode = MODE_CEILING; std::cout << "CEILING MODE" << std::endl; return true;
+	default:
+		switch(mode) {
+		case MODE_OBJECT:
+			if(code == ' ') {
+				if(!artwork.size()) return false;
+				bool next = false;
+				for(artworks_t::iterator m=artwork.begin(); m!=artwork.end(); m++)
+					if(next) {
+						active_model = m->second;
+						next = false;
+						break;
+					} else
+						next = m->second == active_model;
+				if(next)
+					active_model = artwork.begin()->second;
+			}
+			return true;
+		case MODE_FLOOR:
+			return floor->on_key_down(code,map,mouse);
+		case MODE_CEILING:
+			return ceiling->on_key_down(code,map,mouse);
+		default: return false;
+		}		
 	}
-	return false;
 }
 
 bool main_game_t::on_key_up(short code,const input_key_map_t& map,const input_mouse_map_t& mouse) {
@@ -415,10 +477,16 @@ bool main_game_t::on_key_up(short code,const input_key_map_t& map,const input_mo
 	case KEY_RIGHT: pan_rate.x = 0; return true;
 	case KEY_UP:
 	case KEY_DOWN: pan_rate.y = 0; return true;
-	case 's': save(); return true;
-	default: return false;
+	case 's': if(map.none()) save(); return true;
+	default:
+		switch(mode) {
+		case MODE_FLOOR:
+			return floor->on_key_up(code,map,mouse);
+		case MODE_CEILING:
+			return ceiling->on_key_up(code,map,mouse);
+		default: return false;
+		}
 	}
-	return false;
 }
 
 bool main_game_t::on_mouse_down(int x,int y,mouse_button_t button,const input_key_map_t& map,const input_mouse_map_t& mouse) {
@@ -426,8 +494,15 @@ bool main_game_t::on_mouse_down(int x,int y,mouse_button_t button,const input_ke
 	mouse_x = x;
 	mouse_y = y;
 	const int mapped_x = screen_centre.x+mouse_x-width/2, mapped_y = screen_centre.y-mouse_y+height/2;
-	if(mode == MODE_FLOOR)
+	switch(mode) {
+	case MODE_FLOOR:
 		floor->on_mouse_down(mapped_x,mapped_y,button,map,mouse);
+		break;
+	case MODE_CEILING:
+		ceiling->on_mouse_down(mapped_x,mapped_y,button,map,mouse);
+		break;
+	default:;
+	}
 	return true;
 }
 
@@ -441,6 +516,9 @@ bool main_game_t::on_mouse_up(int x,int y,mouse_button_t button,const input_key_
 		break;
 	case MODE_FLOOR:
 		floor->on_mouse_up(mapped_x,mapped_y,button,map,mouse);
+		break;
+	case MODE_CEILING:
+		ceiling->on_mouse_up(mapped_x,mapped_y,button,map,mouse);
 		break;
 	case MODE_OBJECT:
 		if(active_model) {
